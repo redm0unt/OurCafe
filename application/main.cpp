3d57e0d4fb3d1195a0714299d9064a5bda9d0f50
PeaceOfMaster <kirillmaster2020@gmail.com>
2024-03-17 19:41:27 +0300

    Client Backend for application based on Singleton pattern
    Implement interaction with server using client applecation.
    Add backendclient.cpp and backendclient.h to project.
    
    Client application temporally rewritten to console application
    (GUI application implementation commented in main.cpp)

--- application/main.cpp
+++ application/main.cpp
@@ -1,11 +1,36 @@
+// // GUI Application
+// #include "mainwindow.h"
+// #include "backendclient.h"
+
+// #include <QApplication>
+// #include <QString>
+// #include <iostream>
+
+
+// int main(int argc, char* argv[])
+// {
+
+//     QApplication a(argc, argv);
+//     MainWindow w;
+//     w.show();
+//     return a.exec();
+
+// }
+
+// Console Client (Terminal interface only)
 #include "mainwindow.h"
+#include "backendclient.h"
 
-#include <QApplication>
+#include <QCoreApplication>
+#include <QString>
+#include <iostream>
 
-int main(int argc, char *argv[])
+
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QCoreApplication core(argc, argv);
+    std::string message;
+    std::cin >> message;
+    BackendClient::getInstance()->send_message_to_server(QString::fromStdString(message));
+    return core.exec();
 }
