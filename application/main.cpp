3534d79468ae590b3a5517af3699a5b1bb115d65
PeaceOfMaster <kirillmaster2020@gmail.com>
2024-03-25 02:25:29 +0300

    responsive layout sample

--- application/main.cpp
+++ application/main.cpp
@@ -1,36 +1,37 @@
-// // GUI Application
-// #include "mainwindow.h"
-// #include "backendclient.h"
-
-// #include <QApplication>
-// #include <QString>
-// #include <iostream>
-
-
-// int main(int argc, char* argv[])
-// {
-
-//     QApplication a(argc, argv);
-//     MainWindow w;
-//     w.show();
-//     return a.exec();
-
-// }
-
-// Console Client (Terminal interface only)
+// GUI Application
 #include "mainwindow.h"
 #include "backendclient.h"
 
-#include <QCoreApplication>
+#include <QApplication>
+#include <QLabel>
 #include <QString>
 #include <iostream>
 
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication core(argc, argv);
-    std::string message;
-    std::cin >> message;
-    BackendClient::getInstance()->send_message_to_server(QString::fromStdString(message));
-    return core.exec();
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return app.exec();
+
 }
+
+// // Console Client (Terminal interface only)
+// #include "mainwindow.h"
+// #include "backendclient.h"
+
+// #include <QCoreApplication>
+// #include <QString>
+// #include <iostream>
+
+
+// int main(int argc, char* argv[])
+// {
+//     QCoreApplication core(argc, argv);
+//     std::string message;
+//     std::cin >> message;
+//     BackendClient::getInstance()->send_message_to_server(QString::fromStdString(message));
+//     return core.exec();
+// }
