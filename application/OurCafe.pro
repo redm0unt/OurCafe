cbdd0f8ad37295f5afa6a088e3daa769c4ff35cd
PeaceOfMaster <kirillmaster2020@gmail.com>
2024-03-25 05:04:59 +0300

    Simplify structure of blocks and layouts

--- application/OurCafe.pro
+++ application/OurCafe.pro
@@ -1,4 +1,4 @@
-QT += core network
+QT += gui network core
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
--- application/main.cpp
+++ application/main.cpp
@@ -1,4 +1,3 @@
-// GUI Application
 #include "mainwindow.h"
 #include "backendclient.h"
 
@@ -11,27 +10,12 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    MainWindow w;
-    w.show();
+    MainWindow window;
+    window.show();
+
+    // initialize connection with server and send message after drawing GUI
+    BackendClient* interface = BackendClient::getInstance();
+    interface->send_message_to_server(QString::fromStdString("Hi Server!!! I'm application"));
 
     return app.exec();
-
 }
-
-// // Console Client (Terminal interface only)
-// #include "mainwindow.h"
-// #include "backendclient.h"
-
-// #include <QCoreApplication>
-// #include <QString>
-// #include <iostream>
-
-
-// int main(int argc, char* argv[])
-// {
-//     QCoreApplication core(argc, argv);
-//     std::string message;
-//     std::cin >> message;
-//     BackendClient::getInstance()->send_message_to_server(QString::fromStdString(message));
-//     return core.exec();
-// }
