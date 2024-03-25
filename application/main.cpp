b0707dfe194f21b6e43f1f7a3ad7cd104a15391e
PeaceOfMaster <kirillmaster2020@gmail.com>
2024-03-25 04:56:43 +0300

    Combine GUI and Backend of Client

--- application/main.cpp
+++ application/main.cpp
@@ -1,36 +1,21 @@
-// // GUI Application
-// #include "mainwindow.h"
-// #include "backendclient.h"
-
-// #include <QApplication>
-// #include <QString>
-// #include <iostream>
-
-
-// int main(int argc, char* argv[])
-// {
-
-//     QApplication a(argc, argv);
-//     MainWindow w;
-//     w.show();
-//     return a.exec();
-
-// }
-
-// Console Client (Terminal interface only)
 #include "mainwindow.h"
 #include "backendclient.h"
 
-#include <QCoreApplication>
+#include <QApplication>
+#include <QLabel>
 #include <QString>
 #include <iostream>
 
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication core(argc, argv);
-    std::string message;
-    std::cin >> message;
-    BackendClient::getInstance()->send_message_to_server(QString::fromStdString(message));
-    return core.exec();
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+
+    // initialize connection with server and send message after drawing GUI
+    BackendClient* interface = BackendClient::getInstance();
+    interface->send_message_to_server(QString::fromStdString("Hi Server!!! I'm application"));
+
+    return app.exec();
 }
